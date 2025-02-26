c95110e68c34e7f556eef44f8d99ac5a5263daca
MoinZargar <moinbashir2019@gmail.com>
2025-02-26 19:27:04 +0530

    fix api error

--- lib/config/videoNotesAI.ts
+++ lib/config/videoNotesAI.ts
@@ -3,7 +3,7 @@ import { videoNotesPrompt } from "@/lib/constants";
 
 const videoNotesAI = new GoogleGenerativeAI(process.env.GEMINI_VIDEO_NOTES_API_KEY || "");
 const videoNotesModel = videoNotesAI.getGenerativeModel({
-     model: "gemini-1.5-flash", 
+     model: "gemini-2.0-flash-lite", 
      systemInstruction: videoNotesPrompt
     });
 
